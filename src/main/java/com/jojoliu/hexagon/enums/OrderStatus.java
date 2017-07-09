722cba2ca1bc8a59062ccc683ab250a69140cabd
songyilon <yilong.song@qq.com>
2017-07-09 23:50:47 +0800

    add post status

--- src/main/java/com/jojoliu/hexagon/enums/OrderStatus.java
+++ src/main/java/com/jojoliu/hexagon/enums/OrderStatus.java
@@ -1,9 +1,10 @@
 package com.jojoliu.hexagon.enums;
 
 /**
- * Created by Jojo on 25/05/2017.
+ * Created by jojo on 25/05/2017.
  */
 public enum OrderStatus {
+
     I("初始"),
     P("处理中"),
     S("成功"),
---
+++ src/main/java/com/jojoliu/hexagon/enums/PostStatus.java
@@ -0,0 +1,38 @@
+package com.jojoliu.hexagon.enums;
+
+/**
+ * Created by s on 17-7-9.
+ *
+ * 需求状态类型
+ */
+
+
+public enum PostStatus {
+
+    Init("初始状态", -1),
+    Rescinded("已撤销", 0),
+    PendingPublish("待发布",1),
+    PublishFail("发失败", 2),
+    PublishSuccess("发布成功",3),
+    PendingPay("待支付",4),
+    MissedOrder("未接单",5),
+    ReceivedOrder("已接单",6),
+    Completed("已完成",7)
+    ;
+
+    private String msg;
+    private int idx;
+
+    PostStatus(String msg, int idx) {
+        this.msg = msg;
+        this.idx = idx;
+    }
+
+    public String getStatusMsg() {
+        return msg;
+    }
+
+    public int getStatusIdx() {
+        return idx;
+    }
+}
